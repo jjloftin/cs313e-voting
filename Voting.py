bc79a2d14487b87f3339b41bc3f0a24260be102a
jjloftin <johnloftin99@gmail.com>
2014-10-23 17:56:20 -0500

    Finish unit tests

--- Voting.py
+++ Voting.py
@@ -5,14 +5,25 @@
 #ballot class. Holds the preferences of each voter. Can be altered to remove losing 
 #candidates
 class ballot :
-  def __init__(self):
-    self.votes = []
-  def add(self, lst):
-    self.votes += lst
-  def remove(self):
-    self.votes = self.votes[1:]
+  def __init__(self, list =[0]):
+    self.votes = list
+    self.index = 0
+  #allows index to be moved up by one
+  def index_up(self):
+    assert self.index < len(self.votes) - 1
+    self.index += 1
+  def set_index(self, n):
+    assert n < len(self.votes) 
+    assert type(n) == int    
+    self.index = n
+    
+  def vote(self):
+    return self.votes[self.index]
+  def index(self):
+    return self.index
   def __str__(self):    
     return str(self.votes)
+
 #----------------
 #Candidate(Class)
 #----------------
@@ -22,25 +33,26 @@ class candidate :
   def __init__(self, name, votes = 0):
     self.name = name
     self.votes = votes
-    self.ballotList = [] 
-  def voteFor (self, newBallot):
+    self.ballot_list = [] 
+    
+  def vote_for (self, newBallot):
     type(newBallot) is ballot
     self.votes += 1
-    self.ballotList.append(newBallot)
-  def voteTot(self):
+    self.ballot_list.append(newBallot)
+  def vote_tot(self):
     return self.votes
-  def ballotList(self):
-    return self.ballotList
+  def ballot_list(self):
+    return self.ballot_list
   def __str__(self):
     return str(self.name)
 
-#---------------
-#election_set_up
-#---------------
+#-------------
+#election_set
+#-------------
 #Sets up the parameters of the election. Creates a dictionary of candidates and
 #their assigned number and creates a list of ballots
 
-def election_set_up(r,w):
+def election_set(r):
   
   return {}, []
 
@@ -50,16 +62,9 @@ def election_set_up(r,w):
 #Run an austrlian style election returns the name of the winning candidate or 
 #tied candidates
 
-def election_run(r,w):
+def election_solve(r,w):
   
   w.write('')
 
-a = candidate('e')
-print(a)
-b = ballot()
-print(b)
-b.add([1,2,3])
-print(b)
-b.remove()
-print(b)
-print(type(ballot()) is ballot)
+  
+
--- VotingTests.py
+++ VotingTests.py
@@ -5,48 +5,109 @@
 from io       import StringIO
 from unittest import main, TestCase
 
-from Voting import ballot, candidate, election_set_up, election_run
+from Voting import ballot, candidate, election_set, election_solve
 
 class VotingTests(TestCase):
+  #------
+  #ballot
+  #------
   def test_ballot1(self):
-    a = ballot()
-    a.add([1,2,3])
-    print(a)
-    assert(str(a)) == str([1,2,3])
-    
-  def test_ballot2(self):
-    a = ballot()
-    a.add([1,2,3])
-    print(a)
-    a.remove()
-    assert(str(a)) == str([2,3])
+    a = ballot([1,2,3])
+    assert str(a) == str([1,2,3])
    
-  def test_ballot3(self):
-    a = ballot()
-    a.add([1,2,3])
-    a.remove()
-    a.add([4,5,6])
-    print(a)
-    assert(str(a)) == str([2,3,4,5,6])
+  def test_ballot2(self):
+    a = ballot([1,2,3,4,5,6])
+    a.index_up()
+ 
+    assert a.vote() == 2
     
+  def test_ballot3(self):
+    a = ballot([1,2,3,4,5,6])
+    a.set_index(5)
+   
+    assert a.vote() == 6
+  
+  #---------
+  #candidate  
+  #---------
   def test_candidate1(self):
     a = candidate('Adam')
-    a.voteFor(ballot())
+    a.vote_for(ballot())
       
-    assert(a.voteTot() == 1)
+    assert(a.vote_tot() == 1)
       
   def test_candidate2(self):
     a = candidate('Branfordington')
-    a.voteFor(ballot())
-    a.voteFor(ballot())
-    a.voteFor(ballot()) 
+    a.vote_for(ballot())
+    a.vote_for(ballot())
+    a.vote_for(ballot()) 
     
-    assert(a.voteTot() == 3)    
+    assert(a.vote_tot() == 3)    
   
   def test_candidate3(self):
     a= candidate('Bashmanogan')
     assert(str(a) == 'Bashmanogan')
     
+  def test_candidates4(self):
+    a = candidate('Lando Calrissian')
+    b = ballot([1])
+    
+    for i in range(5):
+      a.vote_for(b)
+
+    for item in a.ballot_list:
+      assert str(item) == str([1])
+  
+  #---
+  #set
+  #---  
+  def test_election_set1(self):
+    r = StringIO('1\n\n3\nJohn Doe \n Jane Smith \n Sirhan Sirhan \n 1 2 3 \n 1 2 3 \n 2 1 3\n 2 3 1 \n 1 2 3 \n 3 1 2') 
+    dict, lst = election_set(r)
+    
+    assert {1:'John Doe', 2: 'Jane Smith', 3:'Sirhan Sirhan'} == dict
+    
+  def test_election_set2(self):
+    r = StringIO('1\n\n3\nJohn Doe\nJane Smith\nSirhan Sirhan\n1 2 3\n1 2 3\n2 1 3 \n 2 3 1\n 1 2 3 \n 3 1 2') 
+    dict, lst =  election_set(r)
+    
+    assert [[1,2,3], [1,2,3], [2,1,3], [2,3,1], [1,2,3], [3,1,2]] == lst
+  def test_election_set3(self):
+c
+    dict, lst = election_set(r)
+    
+    assert [[1],[1],[1],[1]] == lst 
+    
+  def test_election_set4(self):
+    r = StringIO('1\n\n1\nLando PandaBears\n1\n1\n1\n1')
+    dict,lst = election_set(r)
+    
+    assert {1: 'Lando PandaBears'} == dict 
+    
+   #-----
+   #Solve
+   #-----
+  def test_electiion_aolve1(self):
+    r = StringIO('1\n\n1\nLando PandaBears\n1\n1\n1\n1')
+    w = StringIO()
+    
+    assert w.getvalue() == 'Lando PandaBears'
+  def test_election_solve2(self):
+    r = StringIO('1\n\n3\nJohn Doe \n Jane Smith \n Sirhan Sirhan \n 1 2 3 \n 1 2 3 \n 2 1 3\n 2 3 1 \n 1 2 3 \n 3 1 2')
+    w = StringIO()
+    
+    assert w.getvalue() == 'John Doe'
+  
+  def test_election_solve3(self):
+    r = StringIO('2\n\n3\nJohn Doe \n Jane Smith \n Sirhan Sirhan \n 1 2 3 \n 1 2 3 \n 2 1 3\n 2 3 1 \n 1 2 3 \n 3 1 2\n\n1\nLando PandaBears\n1\n1\n1\n1')
+    w = StringIO()
+    
+    assert w.getvalue() == 'John Doe\n\nLando PandaBears'
+    
+    
+    
+    
+   
 #----
 #main
 #----
